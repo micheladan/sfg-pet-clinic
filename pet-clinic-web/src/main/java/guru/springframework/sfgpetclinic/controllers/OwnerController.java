663cf5d917caa62ee38fcf0004b89383e0c382c4
Michel Adan <m.adan@rsegrp.com>
2018-12-11 07:19:10 -0800

    Adding web-jars and controllers

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -0,0 +1,27 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by jt on 7/22/18.
+ */
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners(Model model){
+
+        return "owners/index";
+    }
+
+    @RequestMapping("/find")
+    public String findOwners(){
+        return "notimplemented";
+    }
+
+}
---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -0,0 +1,19 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by jt on 7/22/18.
+ */
+@Controller
+public class VetController {
+
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
+    public String listVets(Model model){
+
+        return "vets/index";
+    }
+}
