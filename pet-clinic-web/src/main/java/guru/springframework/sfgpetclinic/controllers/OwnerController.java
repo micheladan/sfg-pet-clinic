2cf752c59017363fd4fc6c4c9f251fe45a11ced1
Michel Adan <m.adan@rsegrp.com>
2018-12-11 14:33:24 -0800

    MockMVC Example for OwnerController

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -18,12 +18,17 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"", "/find", "/index", "/index.html"})
+    @RequestMapping({"", "/", "/index"})
     public String listOwners(Model model){
 
         model.addAttribute("owners", ownerService.findAll());
 
         return "owners/index";
+    }
+
+    @RequestMapping({"find"})
+    public String findOwner(Model model){
+        return "notimplemented";
     }
 
 }
